c0f5741c513c3ba862f889be6ec9fcd0470581af
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2020-12-24 11:59:10 +0900

    usersテーブル仕様変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_081810) do
+ActiveRecord::Schema.define(version: 2020_12_24_025433) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_081810) do
     t.integer "subscribers_num", null: false
     t.string "channel_url", default: "", null: false
     t.text "what_channel", null: false
-    t.string "twitter_url", default: "", null: false
+    t.string "twitter_url", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
