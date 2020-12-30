399837b5e6344dac997b6fc0000562ced9748bb0
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2020-12-30 11:09:50 +0900

    postsコントローラーindexアクション、createアクション修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order("RAND()").limit(9)
   end
   def new
     @post = Post.new
   end
   def create
     @post = Post.new(post_params)
+    url = params[:post][:video_url]
+    url = url.slice(-11,11)
+    @post.video_url = url
     if @post.save
       redirect_to root_path
     else
