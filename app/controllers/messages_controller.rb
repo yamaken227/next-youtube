c0f7ad64b6ce425ec8965e6bbfb6f3765d16f325
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-01-05 15:25:44 +0900

    messagesコントローラ作成

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,11 @@
+class MessagesController < ApplicationController
+  def create
+    Message.create(message_params)
+    redirect_to "/posts/#{message.post.id}"
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:message).merge(post_id: params[:post_id])
+  end
+end
