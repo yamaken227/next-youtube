6592c0b7c7d79d4d5710fcac025b7eedce8e25a4
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-01-12 10:29:36 +0900

    不要ファイル不要記述削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,6 @@ class UsersController < ApplicationController
 #    @posts = @user.posts
     @lists = Kaminari.paginate_array(@user.posts).page(params[:page]).per(6)
   end
-  def edit
-    unless @user.id == current_user.id
-      redirect_to user_path(@user)
-    end
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to user_path(@user.id)
-    else
-      render :edit
-    end
-end
 
   private
 
