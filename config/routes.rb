0c7e29b0e97034802c0a04465386a3a47e285e3f
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-01-13 15:31:43 +0900

    ルートパスの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users,controllers: {
     registrations: "users/registrations"
     }
-  root to: 'posts#index'
+#  root to: 'posts#index'
+  
+  root to: 'posts#home'
   resources :users
   resources :posts do
     resources :messages
