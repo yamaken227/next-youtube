43afc88cc152ff17c5cdbc5ba31f1d7925e9a91e
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-01-18 15:16:38 +0900

    単体テスト項目追加

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :post do
     title          { Faker::Name.initials }
     highlights     { 'あいうえお' }
-    video_url      { 'httt:aaa' }
+    video_url      { 'https://www.youtube.com/watch?v=cCmT1fi7Jla' }
     association :user
   end
 end
