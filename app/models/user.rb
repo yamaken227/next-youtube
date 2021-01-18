efcf3d9e00c706bbf3c46dc7bdfe7ef919fa6d73
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-01-18 18:34:19 +0900

    userモデルバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,17 +17,19 @@ class User < ApplicationRecord
     less_than_or_equal_to: 9999,
     message: 'が10000人以上の方はご利用いただけません'
   }
-
+  
   validates :subscribers_num, numericality: {
     with: /\A[0-9]+\z/, message: 'は半角数字のみ入力可能です'
   }
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は文字と数字の両方を含めてください', on: :create
 
-  #  with_options if: 'password || new_record?' do |user|
-  #    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  #    validates_format_of :password, with: PASSWORD_REGEX, message: 'は文字と数字の両方を含めてください'
-  #  end
+  VALID_YOUTUBECHANNEL_URL = /(\Ahttps:\/\/www\.youtube\.com\/channel\/)/
+  validates_format_of :channel_url, with: VALID_YOUTUBECHANNEL_URL, message: 'はYouTubeのチャンネルURLのみご利用いただけます'
+
+  VALID_TWITTER_URL = /(\Ahttps:\/\/twitter\.com\/)/
+  validates_format_of :twitter_url, with: VALID_TWITTER_URL, message: 'はTwitterのURLのみご利用いただけます' , if: -> { twitter_url.present? }
 
   def update_without_password(params, *options)
     params.delete(:current_password)
