5e1513be371ab6962c0e7af2b270671fb54b69cb
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-01-19 16:36:21 +0900

    ユーザー詳細ページデザイン修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   def show
     #    @posts = @user.posts
-    @lists = Kaminari.paginate_array(@user.posts).page(params[:page]).per(6)
+    @lists = Kaminari.paginate_array(@user.posts).page(params[:page]).per(9)
   end
 
   private
