5a4120750f86e66cf024d2302367040ae6fadc67
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-01-21 16:26:34 +0900

    postモデルバリデーション修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,16 +9,8 @@ class Post < ApplicationRecord
     validates :video_url
   end
 
-  VALID_YOUTUBE_URL = /(\Ahttps:\/\/www\.youtube\.com\/watch\?v=)+[\w]{11}\z/
+#  VALID_YOUTUBE_URL = /(\Ahttps:\/\/www\.youtube\.com\/watch\?v=)+[\w]{11}\z/
+  VALID_YOUTUBE_URL = /\A(https\:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)+[\S]{11}\z/
   validates_format_of :video_url, with: VALID_YOUTUBE_URL, message: 'はYouTubeの動画のURLのみご利用いただけます'
 
-  def self.index(search)
-    if search != ""
-      bidnding.pry
-      Post.where('title LIKE(?)', "%#{search}%")
-    else
-      bidnding.pry
-      Post.all
-    end
-  end
 end
